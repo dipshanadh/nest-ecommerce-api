b4c95325b9451e74500aed6b2e02596027419336
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-02-24 19:15:57 +0545

    refactor: remove ValidateMongoId decorator and use it directly inside @Param()

--- src/utils/mongoId-validation.ts
+++ src/utils/mongoId-validation.ts
@@ -3,21 +3,15 @@ import {
 	Injectable,
 	PipeTransform,
 	BadRequestException,
-	UsePipes,
-	applyDecorators,
 } from "@nestjs/common"
 import { isValidObjectId } from "mongoose"
 
 @Injectable()
-class MongoIdValidationPipe implements PipeTransform {
+export class MongoIdValidationPipe implements PipeTransform {
 	transform(id: any, metadata: ArgumentMetadata) {
 		if (!isValidObjectId(id))
 			throw new BadRequestException(["Invalid MongoID"])
 
 		return id
 	}
-}
-
-export function ValidateMongoId() {
-	return applyDecorators(UsePipes(MongoIdValidationPipe))
 }

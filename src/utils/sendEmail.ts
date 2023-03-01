53a7c720eb74bdb4833b1a03b270ae0ef134c955
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-03-01 21:48:55 +0545

    feat: sendEmail

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,29 @@
+import { createTransport, SendMailOptions } from "nodemailer"
+
+export interface mailOptions {
+	to: string
+	subject: string
+	html: string
+}
+
+export const sendEmail = async (options: mailOptions) => {
+	const { to, subject, html } = options
+
+	const transporter = createTransport({
+		host: process.env.SMTP_HOST,
+		port: Number(process.env.SMTP_PORT),
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	})
+
+	const mailOptions: SendMailOptions = {
+		from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+		to,
+		subject,
+		html,
+	}
+
+	await transporter.sendMail(mailOptions)
+}
